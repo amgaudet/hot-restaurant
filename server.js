60b1725f0fb14223e5f824bcef158ca8cbba4333
Shreekz <alanmguadet@gmail.com>
2021-06-11 14:42:39 -0400

    added page html and directory routing

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
+const reservations = [];
+const waitingList = [];
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Hot Restaurant!');
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, 'reservation.html')));
+
+
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
